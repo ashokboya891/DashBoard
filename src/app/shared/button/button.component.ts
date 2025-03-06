4f49976b6bf017a4b3417e7563926de556787afd
AshokKumar <ashokboya891@gmail.com>
2025-03-06 15:37:36 +0530

    content-projection ng-content

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'button[appButton]',
@@ -9,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class ButtonComponent {
 
+  @Input() type = 'button';
 }
