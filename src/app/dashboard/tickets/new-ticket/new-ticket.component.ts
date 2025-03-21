20213be7bc15dc45dc825f449ee590e54ec2bbbf
AshokKumar <ashokboya891@gmail.com>
2025-03-21 17:07:45 +0530

    upto host intro

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ControlComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  // onSubmit(title: string, ticketText: String) {
+  //   console.log(title);
+  //   console.log(ticketText);
 
+  //   this.form?.nativeElement.reset();
+  // }
 }
--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]',
+  selector: 'button[appButton],a[appButton]', // selector: 'button[appButton].a[button] for achor tag also we can use this',   
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
