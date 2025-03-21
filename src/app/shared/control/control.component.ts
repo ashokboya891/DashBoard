81ca59e191ac91f3c846d15293a53dfc2dd7650b
AshokKumar <ashokboya891@gmail.com>
2025-03-21 17:17:30 +0530

    hostbinder and listner

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,11 +7,23 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
-  host:{
-    class: 'control'
+  host:{  //prefered has to work with onclick inside component
+    class: 'control',
+    '(click)':'onClick()'
   }
 
 })
 export class ControlComponent {
+  // @HostBinding('class') className='control';  //outdated
+  // @HostListener('click') onClick()   //new one but not prefered
+  // {
+  //   console.log('Clicked!!');
+  // }
   label=input.required<string>();
+
+  onClick()      
+  {
+    console.log('Clicked!!');
+  }
 }
+
